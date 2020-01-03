9739b457a52fde154dfe8ac9a713745f41bbf4bb
Gattu <NGattu@ad.harman.com>
2020-01-03 11:38:25 +0530

    Added logs for displaying queue deatils that are sent to azure cloud

--- ImpactSimulator/src/main/java/fb/pricingAnalytics/utils/FBAzureQueuePublisher.java
+++ ImpactSimulator/src/main/java/fb/pricingAnalytics/utils/FBAzureQueuePublisher.java
@@ -1,8 +1,10 @@
 package fb.pricingAnalytics.utils;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.gson.Gson;
+
 
 import fb.pricingAnalytics.events.ImpactSimulatorEvent;
 import fb.pricingAnalytics.model.auth.UserAuth;
@@ -11,17 +13,15 @@ import fb.pricingAnalytics.queue.impl.QueuePublisherImpl;
 
 public class FBAzureQueuePublisher {
 	
-	private static Logger logger = Logger.getLogger(FBAzureQueuePublisher.class);
+	private static Logger logger = LoggerFactory.getLogger(FBAzureQueuePublisher.class);
 	
 	public void sendEventToQueue(ImpactSimulatorEvent event,UserAuth userAuth){
 		try{
 			QueuePublisher queuePublisher = new QueuePublisherImpl();
 			String queueName = FBConstants.PA_QUEUE+userAuth.getBrandId();
 			logger.info("---- queueName ---- " + queueName);
-			logger.info("queueName : " + queueName);
 			Gson gson = new Gson();
 			String json = gson.toJson(event);
-			logger.info("json : " + json);
 			logger.info("----- json -----  " + json);
 			String queueURL = queuePublisher.createQueue(queueName);
 			logger.info("queueURL : " + queueURL);
