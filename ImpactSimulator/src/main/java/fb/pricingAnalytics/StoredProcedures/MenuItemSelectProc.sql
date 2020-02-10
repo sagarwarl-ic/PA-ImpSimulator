f70cc0e8ca8df6374182cae21d774e479955115f
sagarwarl-ic <sagarwarl@fishbowl.com>
2020-02-10 14:04:39 +0530

    d

--- ImpactSimulator/src/main/java/fb/pricingAnalytics/StoredProcedures/MenuItemSelectProc.sql
+++ ImpactSimulator/src/main/java/fb/pricingAnalytics/StoredProcedures/MenuItemSelectProc.sql
@@ -79,6 +79,7 @@ SELECT
 [IST_Store_Product_Info].[Cat1] AS [Cat1],
 [IST_Store_Product_Info].[Cat2] AS [Cat2],
 [IST_Store_Product_Info].[Cat3] AS [Cat3],
+[IST_Store_Product_Info].[Current_Tier] AS [Current_Tier],
 [IST_Store_Product_Info].[Sales_Gross_TY] AS [Sales_Gross_TY],
 [IST_Store_Product_Info].[Quantity_TY] AS [Quantity_TY],
 [IST_Store_Product_Info].[Sales_Gross_LY] AS [Sales_Gross_LY],
@@ -103,9 +104,9 @@ and IST_Store_Info.Scenario_ID =@Scenario_Id
 ) as a LEFT JOIN [dbo].[IST_Product_Tier_Info] AS IST_Product_Tier_Info ON
 (a.BrandId=IST_Product_Tier_Info.BrandId and a.Project_Id=IST_Product_Tier_Info.Project_Id
 and a.Product_ID = IST_Product_Tier_Info.Product_ID
-and a.Proposed_Tier=IST_Product_Tier_Info.Tier)
+and a.Proposed_Tier=IST_Product_Tier_Info.Tier and a.Scenario_ID_Store=IST_Product_Tier_Info.[Scenario_Id])
 ) [Custom SQL Query] where BrandId=@BrandId and Project_Id =@Project_Id
-and Scenario_ID_Store =@Scenario_Id and Scenario_Id_Product=@Scenario_Id
+and Scenario_ID_Store =@Scenario_Id 
 
 GROUP BY 
 [Custom SQL Query].[Cat1],
