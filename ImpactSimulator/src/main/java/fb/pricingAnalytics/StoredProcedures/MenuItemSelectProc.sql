c8180be93c794a588d11b9d35f3f974dfb72291a
sagarwarl-ic <sagarwarl@fishbowl.com>
2020-03-04 12:36:17 +0530

    d

--- ImpactSimulator/src/main/java/fb/pricingAnalytics/StoredProcedures/MenuItemSelectProc.sql
+++ ImpactSimulator/src/main/java/fb/pricingAnalytics/StoredProcedures/MenuItemSelectProc.sql
@@ -1,6 +1,6 @@
 USE [ImpactSimulator]
 GO
-/****** Object:  StoredProcedure [dbo].[MenuitemSelectProc]    Script Date: 2/10/2020 3:33:28 AM ******/
+/****** Object:  StoredProcedure [dbo].[MenuitemSelectProc]    Script Date: 3/4/2020 2:05:40 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -15,6 +15,7 @@ ALTER   PROCEDURE [dbo].[MenuitemSelectProc] @startRowIndex int=0,
 @pageSize int=100,
 @Cat1 VARCHAR(100)=null,
 @Cat2 VARCHAR(100)=null,
+@Cat3 VARCHAR(100)=null,
 @Tier VARCHAR(100) = null,
 @ProductPriceSensitivity VARCHAR(100)=null,
 @SortField VARCHAR(100)='Product_ID', @Direction VARCHAR(100)='ASC',
@@ -26,7 +27,13 @@ BEGIN
 WITH Data_Menu_Item
 AS
 (
-SELECT
+select t0.*,
+t1.*,
+case when [t1].Total_Sales_Gross= 0 then NULL else
+ROUND((([t0].Sales_Impact / [t1].Total_Sales_Gross ) *100),2) END as Total_Impact_Percent
+from
+
+(SELECT
 
 [Custom SQL Query].[Cat1] AS [Cat1],
 [Custom SQL Query].[Cat2] AS [Cat2],
@@ -57,18 +64,20 @@ as Sales_Impact_Percentage ,
 
 SUM([Custom SQL Query].[Sales_Gross_TY]) AS Original_Sales,
 
-ROUND((MIN((CASE WHEN [Custom SQL Query].[Current_Price] = 0 THEN NULL
-ELSE (CAST(([Custom SQL Query].[New_Price] - [Custom SQL Query].[Current_Price]) as float) / [Custom SQL Query].[Current_Price]) END))*100),2)
+ROUND(((CASE WHEN MIN([Custom SQL Query].[Current_Price_product]) = 0 THEN NULL
+ELSE (CAST(MIN([Custom SQL Query].[New_Price]) - MIN([Custom SQL Query].[Current_Price_product]) as float) / MIN([Custom SQL Query].[Current_Price_product])) END)*100),2)
 AS Price_Change_Percent,
-ROUND(MIN(([Custom SQL Query].[New_Price] - [Custom SQL Query].[Current_Price])),2) AS Price_Change,
+ROUND((MIN([Custom SQL Query].[New_Price]) - MIN([Custom SQL Query].[Current_Price_product])),2) AS Price_Change,
 MIN([Custom SQL Query].[New_Price]) AS [New_Price],
-MIN([Custom SQL Query].[Current_Price]) AS [Current_Price],
+MIN([Custom SQL Query].[Current_Price_product]) AS [Current_Price],
 SUM(CAST(([Custom SQL Query].[Quantity_TY]) as BIGINT)) AS [Quantity_TY]
 FROM (
 select a.*,
 IST_Product_Tier_Info.Tier ,
 IST_Product_Tier_Info.[Scenario_Id] as Scenario_Id_Product,
-IST_Product_Tier_Info.[Price] AS [New_Price] from
+IST_Product_Tier_Info.[Price] AS [New_Price],
+IST_Product_Tier_Info.[Current_Price] as Current_Price_product
+from
 (
 SELECT
 [IST_Store_Product_Info].BrandId,
@@ -82,8 +91,6 @@ SELECT
 [IST_Store_Product_Info].[Current_Tier] AS [Current_Tier],
 [IST_Store_Product_Info].[Sales_Gross_TY] AS [Sales_Gross_TY],
 [IST_Store_Product_Info].[Quantity_TY] AS [Quantity_TY],
-[IST_Store_Product_Info].[Sales_Gross_LY] AS [Sales_Gross_LY],
-[IST_Store_Product_Info].[Quantity_LY] AS [Quantity_LY],
 [IST_Store_Product_Info].[Current_Price] AS [Current_Price],
 [IST_Store_Product_Info].[Store_Name] AS [Store_Name],
 [IST_Store_Product_Info].[Market_Name] AS [Market_Name],
@@ -106,9 +113,9 @@ and IST_Store_Info.Scenario_ID =@Scenario_Id
 and a.Product_ID = IST_Product_Tier_Info.Product_ID
 and a.Proposed_Tier=IST_Product_Tier_Info.Tier and a.Scenario_ID_Store=IST_Product_Tier_Info.[Scenario_Id])
 ) [Custom SQL Query] where BrandId=@BrandId and Project_Id =@Project_Id
-and Scenario_ID_Store =@Scenario_Id 
+and Scenario_ID_Store =@Scenario_Id
 
-GROUP BY 
+GROUP BY
 [Custom SQL Query].[Cat1],
 [Custom SQL Query].[Cat2],
 [Custom SQL Query].[Cat3],
@@ -117,27 +124,40 @@ GROUP BY
 (CASE WHEN (UPPER(LTRIM(RTRIM([Custom SQL Query].[Product_Price_Sensitivity]))) = 'ELASTIC') THEN 'High' WHEN
 (UPPER(LTRIM(RTRIM([Custom SQL Query].[Product_Price_Sensitivity]))) = 'INELASTIC') THEN 'Low' WHEN
 (UPPER(LTRIM(RTRIM([Custom SQL Query].[Product_Price_Sensitivity]))) = 'MOD') THEN 'Moderate' ELSE 'NA' END),
-[Custom SQL Query].[Proposed_Tier]
+[Custom SQL Query].[Proposed_Tier]) t0
+
+
+CROSS JOIN (
+Select 
+ISNULL(SUM([IST_Store_Product_Info].[Sales_Gross_TY]), 0) AS Total_Sales_Gross
+
+FROM [dbo].[IST_Store_Product_Info] [IST_Store_Product_Info]
+where [IST_Store_Product_Info].BrandId=@BrandId and [IST_Store_Product_Info].Project_Id=@Project_Id
+) 
+ [t1]
 ),
 
 
 Count_CTE
 AS
 (
-SELECT COUNT(*) AS TotalRows FROM Data_Menu_Item where  
-((Cat1 = ISNULL(@Cat1,Cat1)) OR (@Cat1 is null and Cat1 is null)) 
-AND ((Cat2 = ISNULL(@Cat2,Cat2))OR (@Cat2 is null and Cat2 is null))
-AND ((Proposed_Tier = ISNULL(@Tier,Proposed_Tier))OR (@Tier is null and Proposed_Tier is null)) 
-AND ((Product_Price_Sensitivity = ISNULL(@ProductPriceSensitivity,Product_Price_Sensitivity))OR ( @ProductPriceSensitivity is null and Product_Price_Sensitivity is null))
+SELECT COUNT(*) AS TotalRows FROM Data_Menu_Item where 
+
+(Cat1 IN (select * from dbo.splitString(@Cat1,',')) OR (@Cat1 is null and Cat1 is null) OR (@Cat1 is null and Cat1 = Cat1)) 
+AND (Cat2 IN (select * from dbo.splitString(@Cat2,',')) OR (@Cat2 is null and Cat2 is null) OR (@Cat2 is null and Cat2 = Cat2))
+AND (Cat3 IN (select * from dbo.splitString(@Cat3,',')) OR (@Cat3 is null and Cat3 is null) OR (@Cat3 is null and Cat3 = Cat3))
+AND (Proposed_Tier IN (select * from dbo.splitString(@Tier,',')) OR (@Tier is null and Proposed_Tier is null) OR (@Tier is null and Proposed_Tier = Proposed_Tier))
+AND (Product_Price_Sensitivity IN (select * from dbo.splitString(@ProductPriceSensitivity,',')) OR (@ProductPriceSensitivity is null and Product_Price_Sensitivity is null) OR (@ProductPriceSensitivity  is null and Product_Price_Sensitivity = Product_Price_Sensitivity))
+
 
 )
 SELECT *
 FROM Data_Menu_Item
-CROSS JOIN Count_CTE  where   ((Cat1 = ISNULL(@Cat1,Cat1)) OR (@Cat1 is null and Cat1 is null)) 
-AND ((Cat2 = ISNULL(@Cat2,Cat2))OR (@Cat2 is null and Cat2 is null))
-AND ((Proposed_Tier = ISNULL(@Tier,Proposed_Tier))OR (@Tier is null and Proposed_Tier is null)) 
-AND ((Product_Price_Sensitivity = ISNULL(@ProductPriceSensitivity,Product_Price_Sensitivity))OR ( @ProductPriceSensitivity is null and Product_Price_Sensitivity is null))
-
+CROSS JOIN Count_CTE  where   (Cat1 IN (select * from dbo.splitString(@Cat1,',')) OR (@Cat1 is null and Cat1 is null) OR (@Cat1 is null and Cat1 = Cat1)) 
+AND (Cat2 IN (select * from dbo.splitString(@Cat2,',')) OR (@Cat2 is null and Cat2 is null) OR (@Cat2 is null and Cat2 = Cat2))
+AND (Cat3 IN (select * from dbo.splitString(@Cat3,',')) OR (@Cat3 is null and Cat3 is null) OR (@Cat3 is null and Cat3 = Cat3))
+AND (Proposed_Tier IN (select * from dbo.splitString(@Tier,',')) OR (@Tier is null and Proposed_Tier is null) OR (@Tier is null and Proposed_Tier = Proposed_Tier))
+AND (Product_Price_Sensitivity IN (select * from dbo.splitString(@ProductPriceSensitivity,',')) OR (@ProductPriceSensitivity is null and Product_Price_Sensitivity is null) OR (@ProductPriceSensitivity  is null and Product_Price_Sensitivity = Product_Price_Sensitivity))
   order by  
 CASE WHEN @SortField = 'Product_ID' AND  @Direction = 'DESC' THEN [Product_ID] END DESC,
 CASE WHEN @SortField = 'Product_ID' AND  @Direction != 'DESC' THEN [Product_ID] END,
@@ -146,7 +166,9 @@ CASE WHEN @SortField = 'Product_Name' AND  @Direction != 'DESC' THEN [Product_Na
 CASE WHEN @SortField = 'Cat1' AND  @Direction = 'DESC' THEN [Cat1] END DESC,
 CASE WHEN @SortField = 'Cat1' AND  @Direction != 'DESC' THEN [Cat1] END,
 CASE WHEN @SortField = 'Cat2' AND  @Direction = 'DESC' THEN [Cat2] END DESC,
-CASE WHEN @SortField = 'Cat2' AND  @Direction != 'DESC' THEN [Cat2] END
+CASE WHEN @SortField = 'Cat2' AND  @Direction != 'DESC' THEN [Cat2] END,
+CASE WHEN @SortField = 'Cat3' AND  @Direction = 'DESC' THEN [Cat2] END DESC,
+CASE WHEN @SortField = 'Cat3' AND  @Direction != 'DESC' THEN [Cat2] END
 
 OFFSET @startRowIndex ROWS FETCH NEXT @pageSize ROWS ONLY
 
