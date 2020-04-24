7dc7f0ee7c925073c992cbec02ec34c5b7ec0021
sagarwarl-ic <sagarwarl@fishbowl.com>
2020-04-24 11:40:59 +0530

    s

--- ImpactSimulator/src/main/java/fb/pricingAnalytics/StoredProcedures/CompareScenario.sql
+++ ImpactSimulator/src/main/java/fb/pricingAnalytics/StoredProcedures/CompareScenario.sql
@@ -1,12 +1,11 @@
 USE [ImpactSimulator]
 GO
-
-/****** Object:  StoredProcedure [dbo].[CompareScenario]    Script Date: 1/27/2020 12:23:59 PM ******/
+/****** Object:  StoredProcedure [dbo].[CompareScenario_NEW]    Script Date: 4/24/2020 2:10:15 AM ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
+
 
 
 -- =============================================
@@ -14,9 +13,10 @@ GO
 -- Create date: <Create Date,,>
 -- Description:	<Description,,>
 -- =============================================
-ALTER   PROCEDURE [dbo].[CompareScenario] 
+ALTER     PROCEDURE [dbo].[CompareScenario_NEW] 
 @Project_Id bigint=0,
-@BrandId int=0
+@BrandId int=0,
+@DataEntryId bigint=0
 AS
 BEGIN
 SELECT
@@ -54,6 +54,7 @@ SUM([Custom SQL Query].[Sales_Gross_TY]) AS Original_Sales,
 Min([Custom SQL Query].[Transaction_TY]) as Transaction_TY,
 SUM(CAST(([Custom SQL Query].[Quantity_TY]) as BIGINT)) AS Quantity,
 [Custom SQL Query].BrandId,
+[Custom SQL Query].DataEntryId ,
 [Custom SQL Query].Project_Id ,
 [Custom SQL Query].Scenario_ID_Store,
 [Custom SQL Query].Scenario_Id_Product,
@@ -67,7 +68,7 @@ IST_Product_Tier_Info.[Price] AS [New_Price] from
 (
 SELECT
 [IST_Store_Product_Info].BrandId,
-[IST_Store_Product_Info].Project_Id,
+[IST_Store_Product_Info].DataEntryId,
 [IST_Store_Product_Info].[Store_Code] AS [Store_Code],
 [IST_Store_Product_Info].[Product_ID] AS [Product_ID],
 [IST_Store_Product_Info].[Current_Tier] AS [Current_Tier],
@@ -79,29 +80,33 @@ SELECT
 [IST_Store_Product_Info].[Store_Sensitivity] AS [Store_Sensitivity],
 [IST_Store_Info].[Store_Code] AS [Store_Code (IST_Store_Info)],
 [IST_Store_Info].[Proposed_Tier] AS [Proposed_Tier],
+[IST_Store_Info].Project_Id,
 [IST_Store_Info].[Scenario_ID] AS [Scenario_ID_Store]
 FROM [dbo].[IST_Store_Product_Info] [IST_Store_Product_Info]
 LEFT JOIN [dbo].[IST_Store_Info] [IST_Store_Info] ON
 ([IST_Store_Product_Info].BrandId = [IST_Store_Info].BrandId and
-[IST_Store_Product_Info].Project_Id=[IST_Store_Info].Project_Id and
+[IST_Store_Product_Info].DataEntryId=[IST_Store_Info].DataEntryId and
 [IST_Store_Product_Info].[Store_Code] = [IST_Store_Info].[Store_Code] )
+
+where  [IST_Store_Product_Info].BrandId=@BrandId and [IST_Store_Info].BrandId=@BrandId  and [IST_Store_Product_Info].DataEntryId=@DataEntryId 
+and [IST_Store_Info].Project_Id=@Project_Id
+
 ) as a LEFT JOIN [dbo].[IST_Product_Tier_Info] AS IST_Product_Tier_Info
-ON ( a.BrandId=IST_Product_Tier_Info.BrandId and a.Project_Id=IST_Product_Tier_Info.Project_Id
+ON ( a.BrandId=IST_Product_Tier_Info.BrandId and a.DataEntryId=IST_Product_Tier_Info.DataEntryId
 and a.Product_ID = IST_Product_Tier_Info.Product_ID
-and a.Proposed_Tier=IST_Product_Tier_Info.Tier and a.Scenario_ID_Store=IST_Product_Tier_Info.[Scenario_Id] ) 
+and a.Proposed_Tier=IST_Product_Tier_Info.Tier and a.Project_Id=IST_Product_Tier_Info.Project_Id and a.Scenario_ID_Store=IST_Product_Tier_Info.[Scenario_Id] ) 
 ) as [Custom SQL Query] left join [dbo].[Scenario] as [Scenario] on
 ([Custom SQL Query].BrandId=Scenario.BrandId
-and [Custom SQL Query].Project_Id=[Scenario].Project_Id and [Custom SQL Query].Scenario_ID_Store=[Scenario].ScenarioId 
+and [Custom SQL Query].Scenario_ID_Store=[Scenario].ScenarioId 
 )
 
-WHERE (
-[Custom SQL Query].BrandId=@BrandId AND ([Custom SQL Query].[Project_Id] = @Project_Id))
-
+where [Custom SQL Query].BrandId=@BrandId and [Custom SQL Query].DataEntryId =@DataEntryId and [Custom SQL Query].Project_Id=@Project_Id 
 
 GROUP BY
 [Custom SQL Query].[Store_Code],
 [Custom SQL Query].[Store_Sensitivity] ,
 [Custom SQL Query].BrandId,
+[Custom SQL Query].DataEntryId,
 [Custom SQL Query].Project_Id,
 [Custom SQL Query].Scenario_ID_Store,
 [Custom SQL Query].Scenario_Id_Product,
@@ -110,5 +115,3 @@ GROUP BY
 GROUP BY [Custom SQL Query].[Scenario_ID_Store],
 [Custom SQL Query].Name
 END
-GO
-
