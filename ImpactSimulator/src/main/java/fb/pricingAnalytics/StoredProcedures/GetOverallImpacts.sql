8b5fd7d6940332d5ea35dd82848ceb475de3ce84
sagarwarl-ic <sagarwarl@fishbowl.com>
2020-04-24 11:45:48 +0530

    d

--- ImpactSimulator/src/main/java/fb/pricingAnalytics/StoredProcedures/GetOverallImpacts.sql
+++ ImpactSimulator/src/main/java/fb/pricingAnalytics/StoredProcedures/GetOverallImpacts.sql
@@ -1,15 +1,16 @@
 USE [ImpactSimulator]
 GO
-/****** Object:  StoredProcedure [dbo].[GetOverallImpacts]    Script Date: 2/4/2020 4:08:32 AM ******/
+/****** Object:  StoredProcedure [dbo].[GetOverallImpacts_NEW]    Script Date: 4/24/2020 2:15:00 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-ALTER   PROCEDURE [dbo].[GetOverallImpacts]
+ALTER     PROCEDURE [dbo].[GetOverallImpacts_NEW]
 @Scenario_Id bigint =0,
 @Project_Id bigint=0,
-@BrandId int=0
+@BrandId int=0,
+@DataEntryId bigint=0
 AS 
 BEGIN
 
@@ -52,7 +53,7 @@ IST_Product_Tier_Info.[Price] AS [New_Price] from
 (
 SELECT
 [IST_Store_Product_Info].BrandId,
-[IST_Store_Product_Info].Project_Id,
+[IST_Store_Product_Info].DataEntryId,
 [IST_Store_Product_Info].[Store_Code] AS [Store_Code],
 [IST_Store_Product_Info].[Product_ID] AS [Product_ID],
 [IST_Store_Product_Info].[Product_Name] AS [Product_Name],
@@ -65,18 +66,20 @@ SELECT
 [IST_Store_Product_Info].[Store_Sensitivity] AS [Store_Sensitivity],
 [IST_Store_Info].[Store_Code] AS [Store_Code (IST_Store_Info)],
 [IST_Store_Info].[Proposed_Tier] AS [Proposed_Tier],
+[IST_Store_Info].Project_Id,
 [IST_Store_Info].[Scenario_ID] AS [Scenario_ID_Store]
 FROM [dbo].[IST_Store_Product_Info] [IST_Store_Product_Info]
 LEFT JOIN [dbo].[IST_Store_Info] [IST_Store_Info] ON ([IST_Store_Product_Info].BrandId = [IST_Store_Info].BrandId and
-[IST_Store_Product_Info].Project_Id=[IST_Store_Info].Project_Id and [IST_Store_Product_Info].[Store_Code] = [IST_Store_Info].[Store_Code])
+[IST_Store_Product_Info].DataEntryId=[IST_Store_Info].DataEntryId and [IST_Store_Product_Info].[Store_Code] = [IST_Store_Info].[Store_Code])
 where
 [IST_Store_Product_Info].BrandId=@BrandId
 and [IST_Store_Info].BrandId=@BrandId
-and [IST_Store_Product_Info].Project_Id=@Project_Id
+and [IST_Store_Product_Info].DataEntryId=@DataEntryId
+and [IST_Store_Info].Project_Id=@Project_Id
 and IST_Store_Info.Scenario_ID =@Scenario_Id
-) as a LEFT JOIN [dbo].[IST_Product_Tier_Info] AS IST_Product_Tier_Info ON (a.BrandId=IST_Product_Tier_Info.BrandId and a.Project_Id=IST_Product_Tier_Info.Project_Id
-and a.Product_ID = IST_Product_Tier_Info.Product_ID and a.Proposed_Tier=IST_Product_Tier_Info.Tier and a.Scenario_ID_Store=IST_Product_Tier_Info.[Scenario_Id])
-) [Custom SQL Query] where BrandId=@BrandId and Project_Id =@Project_Id and Scenario_ID_Store =@Scenario_Id 
+) as a LEFT JOIN [dbo].[IST_Product_Tier_Info] AS IST_Product_Tier_Info ON (a.BrandId=IST_Product_Tier_Info.BrandId and a.DataEntryId=IST_Product_Tier_Info.DataEntryId
+and a.Product_ID = IST_Product_Tier_Info.Product_ID and a.Proposed_Tier=IST_Product_Tier_Info.Tier  and a.Project_Id=IST_Product_Tier_Info.Project_Id and a.Scenario_ID_Store=IST_Product_Tier_Info.[Scenario_Id])
+) [Custom SQL Query] where BrandId=@BrandId and DataEntryId =@DataEntryId and Project_Id=@Project_Id and  Scenario_ID_Store =@Scenario_Id 
 GROUP BY
 [Custom SQL Query].[Current_Tier],
 [Custom SQL Query].[Proposed_Tier],
