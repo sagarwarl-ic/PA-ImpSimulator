43f8bfd48d9c5a261dac4c0776a3351043b93a29
sagarwarl-ic <sagarwarl@fishbowl.com>
2020-04-24 11:50:06 +0530

    d

--- ImpactSimulator/src/main/java/fb/pricingAnalytics/StoredProcedures/StoreTierViewProc.sql
+++ ImpactSimulator/src/main/java/fb/pricingAnalytics/StoredProcedures/StoreTierViewProc.sql
@@ -1,6 +1,6 @@
 USE [ImpactSimulator]
 GO
-/****** Object:  StoredProcedure [dbo].[StoreTierViewProc]    Script Date: 3/18/2020 1:47:34 AM ******/
+/****** Object:  StoredProcedure [dbo].[StoreTierViewProc_NEW]    Script Date: 4/24/2020 2:19:13 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -11,14 +11,15 @@ GO
 -- Description:	<Description,,>
 -- =============================================
 
-ALter     PROCEDURE  [dbo].[StoreTierViewProc]  @startRowIndex int=0 ,
+ALTER       PROCEDURE  [dbo].[StoreTierViewProc_NEW]  @startRowIndex int=0 ,
 	@pageSize int=100,@CurrentTier VARCHAR(100) = null,
 	@StoreSensitivity VARCHAR(100)=null,@PricingPower VARCHAR(100)=null,
 	@SortField VARCHAR(100)='Store_Code', @Direction VARCHAR(100)='ASC',
 	@Scenario_Id bigint =0,
 	@Project_Id bigint=0,
 	@BrandId int=0,
-	@IsChanged Bit=null
+	@IsChanged Bit=null,
+	@DataEntryId bigint=0
 AS
 BEGIN
 WITH Data_Store_View
@@ -56,7 +57,7 @@ END
 )*100),2)
 as Sales_Impact_Percentage ,
 
-SUM([Custom SQL Query].[Sales_Gross_TY]) AS Original_Sales,
+ROUND(SUM([Custom SQL Query].[Sales_Gross_TY]),0) AS Original_Sales,
 SUM(CAST(([Custom SQL Query].[Quantity_TY]) as BIGINT)) AS Quantity,
 
 Min([Custom SQL Query].[Transaction_TY]) as Original_Transaction,
@@ -117,7 +118,7 @@ from
 (
 SELECT
 [IST_Store_Product_Info].BrandId,
-[IST_Store_Product_Info].Project_Id,
+[IST_Store_Product_Info].DataEntryId,
 [IST_Store_Product_Info].[Store_Code] AS [Store_Code],
 [IST_Store_Product_Info].[Product_ID] AS [Product_ID],
 [IST_Store_Product_Info].[Product_Name] AS [Product_Name],
@@ -137,16 +138,18 @@ SELECT
 [IST_Store_Info].[Store_Code] AS [Store_Code (IST_Store_Info)],
 [IST_Store_Info].[Proposed_Tier] AS [Proposed_Tier],
 [IST_Store_Info].[Scenario_ID] AS [Scenario_ID_Store],
+[IST_Store_Info].Project_Id,
 [IST_Store_Info].isChanged
 FROM [dbo].[IST_Store_Product_Info] [IST_Store_Product_Info]
 LEFT JOIN [dbo].[IST_Store_Info] [IST_Store_Info] ON ([IST_Store_Product_Info].BrandId = [IST_Store_Info].BrandId and
-[IST_Store_Product_Info].Project_Id=[IST_Store_Info].Project_Id and [IST_Store_Product_Info].[Store_Code] = [IST_Store_Info].[Store_Code])
-where [IST_Store_Product_Info].BrandId=@BrandId and [IST_Store_Info].BrandId=@BrandId and [IST_Store_Product_Info].Project_Id=@Project_Id
+[IST_Store_Product_Info].DataEntryId=[IST_Store_Info].DataEntryId and [IST_Store_Product_Info].[Store_Code] = [IST_Store_Info].[Store_Code])
+where [IST_Store_Product_Info].BrandId=@BrandId and [IST_Store_Info].BrandId=@BrandId and [IST_Store_Product_Info].DataEntryId=@DataEntryId
+and [IST_Store_Info].Project_Id=@Project_Id
 and IST_Store_Info.Scenario_ID =@Scenario_Id
 
-) as a LEFT JOIN [dbo].[IST_Product_Tier_Info] AS IST_Product_Tier_Info ON (a.BrandId=IST_Product_Tier_Info.BrandId and a.Project_Id=IST_Product_Tier_Info.Project_Id
-and a.Product_ID = IST_Product_Tier_Info.Product_ID and a.Proposed_Tier=IST_Product_Tier_Info.Tier and a.Scenario_ID_Store=IST_Product_Tier_Info.[Scenario_Id])
-) [Custom SQL Query] where BrandId=@BrandId and Project_Id =@Project_Id
+) as a LEFT JOIN [dbo].[IST_Product_Tier_Info] AS IST_Product_Tier_Info ON (a.BrandId=IST_Product_Tier_Info.BrandId and a.DataEntryId=IST_Product_Tier_Info.DataEntryId
+and a.Product_ID = IST_Product_Tier_Info.Product_ID and a.Proposed_Tier=IST_Product_Tier_Info.Tier  and a.Project_Id=IST_Product_Tier_Info.Project_Id and a.Scenario_ID_Store=IST_Product_Tier_Info.[Scenario_Id])
+) [Custom SQL Query] where BrandId=@BrandId and DataEntryId =@DataEntryId and Project_Id=@Project_Id 
 and Scenario_ID_Store =@Scenario_Id
 GROUP BY (CASE WHEN ([Custom SQL Query].[Store_Sensitivity] >= 0) THEN 'Low' WHEN ([Custom SQL Query].[Store_Sensitivity] <= -1) THEN 'High' ELSE 'Moderate' END),
 (CASE WHEN ([Custom SQL Query].[Current_Tier] = [Custom SQL Query].[Proposed_Tier]) THEN 'N' ELSE 'Y' END),
