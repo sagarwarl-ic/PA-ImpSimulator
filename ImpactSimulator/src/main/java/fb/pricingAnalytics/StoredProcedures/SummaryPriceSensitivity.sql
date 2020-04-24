6d478b133cef79aa03eebee2884b79814bc718ad
sagarwarl-ic <sagarwarl@fishbowl.com>
2020-04-24 11:53:45 +0530

    d

--- ImpactSimulator/src/main/java/fb/pricingAnalytics/StoredProcedures/SummaryPriceSensitivity.sql
+++ ImpactSimulator/src/main/java/fb/pricingAnalytics/StoredProcedures/SummaryPriceSensitivity.sql
@@ -1,6 +1,6 @@
 USE [ImpactSimulator]
 GO
-/****** Object:  StoredProcedure [dbo].[SummaryPriceSensitivity_NEW]    Script Date: 4/24/2020 2:22:40 AM ******/
+/****** Object:  StoredProcedure [dbo].[SummaryPriceSensitivity_NEW]    Script Date: 4/13/2020 11:01:28 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -52,14 +52,10 @@ SELECT [IST_Store_Product_Info].BrandId,
 [IST_Store_Product_Info].DataEntryId,
 [IST_Store_Product_Info].[Store_Code] AS [Store_Code],
 [IST_Store_Product_Info].[Product_ID] AS [Product_ID],
-[IST_Store_Product_Info].[Product_Name] AS [Product_Name],
-[IST_Store_Product_Info].[Current_Tier] AS [Current_Tier],
 [IST_Store_Product_Info].[Sales_Gross_TY] AS [Sales_Gross_TY],
 [IST_Store_Product_Info].[Quantity_TY] AS [Quantity_TY],
 [IST_Store_Product_Info].[Current_Price] AS [Current_Price],
-[IST_Store_Product_Info].[Pricing_Power] AS [Pricing_Power],
 [IST_Store_Product_Info].[Product_Price_Sensitivity] AS [Product_Price_Sensitivity],
-[IST_Store_Product_Info].[Store_Sensitivity] AS [Store_Sensitivity],
 [IST_Store_Info].[Store_Code] AS [Store_Code (IST_Store_Info)],
 [IST_Store_Info].[Proposed_Tier] AS [Proposed_Tier],
 [IST_Store_Info].Project_Id,
@@ -91,46 +87,12 @@ GROUP BY
 ) [t0]
 
 
-
 CROSS JOIN (
 
-SELECT SUM(CAST(([Custom SQL Query].[Quantity_TY]) AS FLOAT) ) AS Total_Quantity,
-ISNULL(SUM([Custom SQL Query].[Sales_Gross_TY]), 0) AS Total_Sales_Gross
-FROM (
-select a.*,
-IST_Product_Tier_Info.Tier ,
-IST_Product_Tier_Info.[Scenario_Id] as Scenario_Id_Product,
-IST_Product_Tier_Info.[Price] AS [New_Price] from
-(
-SELECT[IST_Store_Product_Info].BrandId,
-[IST_Store_Product_Info].DataEntryId,
-[IST_Store_Product_Info].[Store_Code] AS [Store_Code],
-[IST_Store_Product_Info].[Product_ID] AS [Product_ID],
-[IST_Store_Product_Info].[Product_Name] AS [Product_Name],
-[IST_Store_Product_Info].[Current_Tier] AS [Current_Tier],
-[IST_Store_Product_Info].[Sales_Gross_TY] AS [Sales_Gross_TY],
-[IST_Store_Product_Info].[Quantity_TY] AS [Quantity_TY],
-[IST_Store_Product_Info].[Current_Price] AS [Current_Price],
-[IST_Store_Product_Info].[Pricing_Power] AS [Pricing_Power],
-[IST_Store_Product_Info].[Product_Price_Sensitivity] AS [Product_Price_Sensitivity],
-[IST_Store_Product_Info].[Store_Sensitivity] AS [Store_Sensitivity],
-[IST_Store_Info].[Store_Code] AS [Store_Code (IST_Store_Info)],
-[IST_Store_Info].[Proposed_Tier] AS [Proposed_Tier],
-[IST_Store_Info].Project_Id,
-[IST_Store_Info].[Scenario_ID] AS [Scenario_ID_Store]
-FROM [dbo].[IST_Store_Product_Info] [IST_Store_Product_Info]
-LEFT JOIN [dbo].[IST_Store_Info] [IST_Store_Info] ON ([IST_Store_Product_Info].BrandId = [IST_Store_Info].BrandId and
-[IST_Store_Product_Info].DataEntryId=[IST_Store_Info].DataEntryId and [IST_Store_Product_Info].[Store_Code] = [IST_Store_Info].[Store_Code])
-where  [IST_Store_Product_Info].BrandId=@BrandId and [IST_Store_Info].BrandId=@BrandId  and [IST_Store_Product_Info].DataEntryId=@DataEntryId 
-and [IST_Store_Info].Project_Id=@Project_Id  
-and IST_Store_Info.Scenario_ID =@Scenario_Id 
-
-) as a LEFT JOIN [dbo].[IST_Product_Tier_Info] AS IST_Product_Tier_Info ON (a.BrandId=IST_Product_Tier_Info.BrandId and a.DataEntryId=IST_Product_Tier_Info.DataEntryId
-and a.Product_ID = IST_Product_Tier_Info.Product_ID and a.Proposed_Tier=IST_Product_Tier_Info.Tier  and a.Project_Id=IST_Product_Tier_Info.Project_Id  and a.Scenario_ID_Store=IST_Product_Tier_Info.[Scenario_Id])
-) [Custom SQL Query] where  BrandId=@BrandId and DataEntryId =@DataEntryId
-and Project_Id=@Project_Id  
-and Scenario_ID_Store =@Scenario_Id 
-GROUP BY ()
+SELECT SUM(CAST(([IST_Store_Product_Info].[Quantity_TY]) AS FLOAT) ) AS Total_Quantity,
+ISNULL(SUM([IST_Store_Product_Info].[Sales_Gross_TY]), 0) AS Total_Sales_Gross
+FROM  [dbo].[IST_Store_Product_Info] [IST_Store_Product_Info]
+where [IST_Store_Product_Info].BrandId=@BrandId and [IST_Store_Product_Info].DataEntryId=@DataEntryId
 ) [t1])
 Select * from Product_price_sensitivity
 union all
